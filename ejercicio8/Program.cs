92e539190028dc74234d6c01945d654d018da5bf
Jonathan Garcia <jonatan.61191@hotmail.com>
2023-08-30 18:56:48 -0500

    fin de programa

--- ejercicio8/Program.cs
+++ ejercicio8/Program.cs
@@ -1,2 +1,33 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+//Construir un programa que pida un número y luego diga si este número es par o no
+
+namespace Ejercicio8
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero1, numero2, resuldiv, resulmul;
+
+            Console.WriteLine("ingresa el numero 1: ");
+            numero1 = Convert.ToInt32 (Console.ReadLine());
+
+            resuldiv = numero1 / 2;
+            resulmul = resuldiv * 2;
+
+            if (numero1 == resulmul)
+            {
+                Console.Write("El numero " + numero1 + " es un numero Par");
+                Console.Write("\n");
+            }
+
+            else {
+
+                Console.Write("El numero " + numero1 + " es un numero Inpar");
+                Console.Write("\n");
+            }
+
+        }
+    }
+}
