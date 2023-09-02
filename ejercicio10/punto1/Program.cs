ee772de5862779362bd8cd0be36ef51239aaccbd
Jonathan Garcia <jonatan.61191@hotmail.com>
2023-09-01 21:02:25 -0500

    contruccion de la opracion

--- ejercicio10/punto1/Program.cs
+++ ejercicio10/punto1/Program.cs
@@ -9,9 +9,33 @@ namespace Ejercicio
     {
         static void Main(string[] args)
         {
-            Random rnd = new Random();
+            double resultadoX, vriableInterna, parentesis1, parentesis2,parentesis3,parentesis4,corchete;
+            
+            //variable A
+            Random rnda = new Random();
 
-            int ramdomA = rnd.Next(2, 8);
+            int ramdomA = rnda.Next(1, 8);
+
+            //variable B
+            Random rndb = new Random();
+
+            int ramdomB = rndb.Next(1, 8);
+
+            //variable C
+            Random rndc = new Random();
+
+            int ramdomC = rndc.Next(1, 9);
+
+
+            parentesis1 = (3 * ramdomA) + (4 * ramdomB);
+            corchete = Math.Pow(parentesis1, 3);
+            parentesis3 = ramdomC * (Math.Pow(corchete, 2));
+            parentesis2 = (2 * ramdomC) + (ramdomA);
+            parentesis4 = Math.Pow(parentesis2, 2);
+            vriableInterna = parentesis3 / parentesis4; 
+            resultadoX = Math.Sqrt(vriableInterna);
+
+
 
 
 
