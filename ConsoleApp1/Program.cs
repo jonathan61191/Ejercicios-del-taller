f332eb145c0d90f6f4d1c5c5192a6045e513595e
Jonathan Garcia <jonatan.61191@hotmail.com>
2023-09-20 19:23:14 -0500

    ejercicio10

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+//Construir un algoritmo que pida N números por pantalla, cuando el usuario ingrese un número negativo, el programa debe finalizar
+//y mostrar en consola la cantidad total de números positivos que ingresó hasta antes de ingresarse el número negativo
+
+namespace Ejercicio
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            int n, num;
+            double contador = 0;
+
+            Console.WriteLine("Por favor ingrese la cantidad de numeros que desea evaluar");
+            n= Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Por favor ingrese un numero");
+            num = Convert.ToInt32(Console.ReadLine());
+
+            while (num > 0)
+            {
+                contador = contador + 1;
+
+                if (contador < n)
+                {
+                    
+                    Console.WriteLine("Por favor ingrese un numero");
+                    num = Convert.ToInt32(Console.ReadLine());
+
+
+                }
+
+                else break;
+
+                
+            }
+
+            Console.WriteLine(string.Format( "la cantidad de numeros ingresada es:{0} ",contador));
+
+
+
+
+        }
+
+            
+
+
+        
+    }
+}
