f815a63c462d0ec1baab8423e1690d49a7af2d02
Jonathan Garcia <jonatan.61191@hotmail.com>
2023-09-21 16:06:29 -0500

    terminado

--- ejercicio 13/ConsoleApp1/Program.cs
+++ ejercicio 13/ConsoleApp1/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 
-//Comentario
+//Crear un programa que pida un número, hacer la suma de los números naturales igual e
+//inferiores a dicho número ingresado y mostrar el resultado. DONE
 
 namespace Ejercicio
 {
@@ -8,8 +9,23 @@ namespace Ejercicio
     {
         static void Main(string[] args)
         {
+            
+            //Declaracion de variables
+            double numero, resulSuma=0,resulSuma2=0;
 
+            //pedir el numero por pantalla
+            Console.Write("Ingrese el numero que dese sumar: ");
+            numero = Convert.ToDouble(Console.ReadLine());
 
+            while (resulSuma < numero)  
+            {
+                resulSuma++;
+
+                resulSuma2 = resulSuma2 +resulSuma;
+                    };
+
+            Console.Write(String.Format("la suma de todos los numeros hasta este numero es: {0} ", resulSuma2));
+            //Console.Write("Ingrese el numero que dese sumar: ");
         }
     }
 }
