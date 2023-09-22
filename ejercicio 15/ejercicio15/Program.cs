6e73c2225a2e221e33b31dbfe35d76452c9fa1c0
Jonathan Garcia <jonatan.61191@hotmail.com>
2023-09-21 19:08:17 -0500

    terminado

--- ejercicio 15/ejercicio15/Program.cs
+++ ejercicio 15/ejercicio15/Program.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 
-//Comentario
+//Realizar un programa en C# que obtenga el promedio de calificaciones de un grupo de N alumnos.
 
 namespace Ejercicio
 {
@@ -8,6 +8,31 @@ namespace Ejercicio
     {
         static void Main(string[] args)
         {
+
+            //decplaracion de variables
+
+            int n;
+
+            double promedio, nota, notaAcomulado=0  ;
+
+            Console.Write("Por favor ingresar el numero de estudiantes: ");
+            n = Convert.ToInt32(Console.ReadLine());
+
+
+            for (int i = 1; i <= n; i++)
+            {
+                Console.Write("Por favor ingresar la nota del estudiante: ");
+                nota = Convert.ToDouble(Console.ReadLine());
+
+                notaAcomulado += nota;
+            }
+
+            promedio = notaAcomulado / n;
+            Console.Write("\n");
+            Console.WriteLine(String.Format("La nota promedio de los {0} estudiantes es: {0} ", n, promedio));
+
+
+
 
 
         }
