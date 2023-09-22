fe110128513cc24bd5268662f9381b8a705e5d77
Jonathan Garcia <jonatan.61191@hotmail.com>
2023-09-21 22:29:26 -0500

    declaracion de variable

--- ejercicio 16/ejercicio16/Program.cs
+++ ejercicio 16/ejercicio16/Program.cs
@@ -8,6 +8,16 @@ namespace Ejercicio
     {
         static void Main(string[] args)
         {
+            //declarar variables
+
+            double tasa;
+            decimal ahorro, ahorroAño, rendimiento, rendimientoTotal, bono, bonoaño;
+            
+
+            for (int i = 1; i <= 12, i++ )
+            {
+
+            }
 
 
         }
